9f241f8d4033a258d158610b0c9cece04d8e4837
sagarnem <sagarnemkul7@gmail.com>
2024-07-02 09:07:55 +0545

    models

--- auther/models.py
+++ auther/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    nickname = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    nationality = models.CharField(max_length=200, null=True, blank=True)
+    phone = models.PositiveIntegerField()
+    email = models.EmailField()
+    department = models.CharField(max_length=100, null=True, blank=True)
+    education = models.CharField(max_length=40, null=True, blank=True)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+
+
+
+
--- book/models.py
+++ book/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from auther.models import Author
 
 # Create your models here.
+
+class Publication(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    phone = models.PositiveIntegerField()
+    email = models.EmailField()
+    website = models.URLField()
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+    
+class genre(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    
+class Book(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    file = models.FileField(upload_to='book/')
+    image = models.ImageField(upload_to='book-image', null=True, blank=True)
+    publication = models.ForeignKey(Publication, on_delete=models.RESTRICT)
+    genre = models.ManyToManyField(genre)
+    author = models.ManyToManyField(Author)
+    edition = models.PositiveIntegerField()
+    ISBN_number = models.CharField(null=True, blank=True, max_length=40)
+    page_number = models.IntegerField()
+    description = models.CharField(max_length=255, null=True, blank=True)
+    demo_text = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    volume = models.IntegerField()
+    language = models.CharField(max_length=10, null=True, blank=True)
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)  
+    modified_at = models.DateTimeField(auto_now=True)
